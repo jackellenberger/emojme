1edc1176144ca4b0308d5a5bca3c461de2dda893
jackellenberger <jellenberger@uchicago.edu>
2018-09-12 22:34:28 -0500

    add support for one way syncs

--- README.md
+++ README.md
@@ -28,9 +28,9 @@ sync                     get emoji statistics for given user on given subdomain
     * Src json should contain a list of objects where each object contains a "name" and "url" for image source
     * If adding an alias, url will be ignored and "is_alias" should be set to "1", and "alias_for" should be the name of the emoji to be aliased.
 * For user-stats, provide one or several users to get stats on, or give the number of top n emoji contributors.
-* For sync, provide either at least two subdomain, token pairs, or a --from-subdomain, --from-token pair and a --to-subdomain --to-token pair.
+* For sync, provide either at least two subdomain, token pairs, or a --src-subdomain, --src-token pair and a --dst-subdomain --dst-token pair.
     * All subdomains will be updated to have all emoji from all subdomains given if no from and to are specified.
-    * If from and to subdomains are provided, all emoji fromt the from subdomain will be uploaded to the to subdomain.
+    * If src and dst subdomains are provided, all emoji from the src subdomain will be uploaded to the to subdomain.
     * No emoji will ever be overwritten.
 * If for any reason you do not want to use a "cached" version of an emoji list, provide the --no-cache option to force a redownload. Otherwise, data will be redownloaded after 24 hours of staleness.
 
@@ -41,15 +41,41 @@ sync                     get emoji statistics for given user on given subdomain
 ## Examples
 
 #### Download all emoji from subdomain
-`./emoji.js download --subdomain <subdomain> --token <token>`
+`./emoji.js download --subdomain $SUBDOMAIN --token $TOKEN`
 
 #### Download all emoji from multiple subdomains
-`./emoji.js download --subdomain <subdomain> --token <token> --subdomain <subdomain2> --token <token2>`
+`./emoji.js download --subdomain $SUBDOMAIN --token $TOKEN --subdomain $SUBDOMAIN2 --token $TOKEN2`
 
 #### upload emoji from source json to subdomain
-`./emoji.js download --subdomain <subdomain> --token <token> --subdomain <subdomain2> --token <token2>`
+`./emoji.js upload --subdomain $SUBDOMAIN --token $TOKEN --src './myfile.json'`
+
+#### upload emoji from source json to multiple subdomains
+`./emoji.js upload --subdomain $SUBDOMAIN --token $TOKEN --subdomain $SUBDOMAIN2 --token $TOKEN2 --src './myfile.json'`
+
+#### get user statistics for user $USER (emoji upload count, etc)
+`./emoji.js user-stats --subdomain $SUBDOMAIN --token $TOKEN --user $USER`
+
+#### get user statistics for multiple users
+`./emoji.js user-stats --subdomain $SUBDOMAIN --token $TOKEN --user $USER --user $USER2 --user $USER3`
+
+#### get user statistics for top $N contributors
+`./emoji.js user-stats --subdomain $SUBDOMAIN --token $TOKEN --top $N`
+
+#### sync emoji so that $SUBDOMAIN1 and $SUBDOMAIN2 have the same emoji*
+<sup>*the same emoji names, that is. If :hi: is different on the two subdomains they will remain different</sup>
+`./emoji.js sync --subdomain $SUBDOMAIN1 --token $TOKEN1 --subdomain $SUBDOMAIN2 --token $TOKEN2`
+
+#### sync emoji from $SUBDOMAIN1 to $SUBDOMAIN2
+`./emoji.js sync --src-subdomain $SUBDOMAIN1 --src-token $TOKEN1 --dst-subdomain $SUBDOMAIN2 --dst-token $TOKEN2`
 
 ## Finding a slack token
+
+It's easy! Open any signed in slack window, e.g. subdomain.slack.com/messages, right click anywhere > inspect element. Open the console and paste:
+```
+window.prompt("your api token is: ",/api_token: "(.*)"/.exec(document.body.innerHTML)[1])
+```
+You will be prompted with your api token! From what I can tell these last anywhere from a few days to indefinitely. Currently, user tokens follow the format:
+`xoxs-(\w{12}|\w{10})-(\w{12}|\w{11})-\w{12}-\w{64}` but admittedly I have a small sample size.
 
 ## Inspirations
 * [emojipacks](https://github.com/lambtron/emojipacks) is my OG. It mostly worked but seems rather undermaintained.
--- emoji.js
+++ emoji.js
@@ -12,8 +12,19 @@ function list(val, memo) {
   return memo;
 }
 
+function hasValidSubdomainInputs(program) {
+  return program.subdomain && program.token && program.subdomain.length === program.token.length;
+}
+
+function hasValidSrcDstInputs(program) {
+  return program.srcSubdomain && program.srcToken &&
+    program.dstSubdomain && program.dstToken &&
+    program.srcSubdomain.length === program.srcToken.length &&
+    program.dstSubdomain.length === program.dstToken.length;
+}
+
 function main() {
-  let adminList;
+  let adminList, authPairs;
 
   program
     .version(require('./package').version)
@@ -24,59 +35,90 @@ function main() {
     .option('-s, --subdomain [value]', 'slack subdomain. Can be specified multiple times, paired with respective token.', list, null)
     .option('-t, --token [value]', 'slack user token. ususaly starts xoxp-... Can be specified multiple times, paired with respective subdomains.', list, null)
     .option('--src [value]', 'source file(s) for emoji json you\'d like to upload', list, null)
+    .option('--src-subdomain [value]', 'subdomain from which to draw emoji for one way sync', list, null)
+    .option('--src-token [value]', 'token with which to draw emoji for one way sync', list, null)
+    .option('--dst-subdomain [value]', 'subdomain to which to emoji will be added is one way sync', list, null)
+    .option('--dst-token [value]', 'token with which emoji will be added for one way sync', list, null)
     .option('--user [value]', 'slack user you\'d like to get stats on. Can be specified multiple times for multiple users.', list, null)
     .option('--top [value]', 'the top n users you\'d like user emoji statistics on', 10)
     .option('--no-cache', 'force a redownload of all cached info.')
     .parse(process.argv)
 
-  if (!program.subdomain || !program.token || program.subdomain.length != program.token.length) {
-    return Promise.reject('At least one subdomain/token pair is required');
-  }
-
-  let authPairs = _.zipWith(program.subdomain, program.token, (s, t) => {
-    return {subdomain: s, token: t};
-  });
-
-  if (program.download) {
-    return Promise.all(authPairs.map((authPair) => {
-      adminList = new EmojiAdminList(authPair.subdomain, authPair.token);
-      return adminList.get();
-    }));
-  }
-  if (program.upload) {
-    if (!program.src) {
-      return Promise.reject('Required option --src not specified');
-    }
-    return Promise.all(authPairs.map((authPair) => {
-      emojiAdd = new EmojiAdd(authPair.subdomain, authPair.token);
-      return emojiAdd.upload(program.src);
-    }));
-  }
-  if (program.userStats) {
-    return Promise.all(authPairs.map((authPair) => {
-      adminList = new EmojiAdminList(authPair.subdomain, authPair.token);
-      return adminList.get().then(emojiList => {
-        //TODO: update summarizeUser to take single user for consistency?
-        if (program.user) {
-          adminList.summarizeUser(emojiList, program.user);
-        } else {
-          adminList.summarizeSubdomain(emojiList, program.top);
-        }
-      });
-    }));
-  }
-  if (program.sync) {
-    if (program.subdomain.length < 2 || program.subdomain.length != program.token.length) {
-      return Promise.reject('Sync requires pairs of subdomain / token arguments');
-    }
-
-    return Promise.all(authPairs.map((authPair) => {
-      adminList = new EmojiAdminList(authPair.subdomain, authPair.token);
-      return adminList.get();
-    })).then((lists) => {
-      let diffsToUpload = EmojiAdminList.diff(lists, program.subdomain);
-      Promise.resolve(diffsToUpload);
+  if (hasValidSubdomainInputs(program)) {
+    authPairs = _.zipWith(program.subdomain, program.token, (s, t) => {
+      return {subdomain: s, token: t};
     });
+
+    if (program.download) {
+      return Promise.all(authPairs.map((authPair) => {
+        adminList = new EmojiAdminList(authPair.subdomain, authPair.token);
+        return adminList.get();
+      }));
+    }
+
+    if (program.upload) {
+      if (!program.src) {
+        return Promise.reject('Required option --src not specified');
+      }
+      return Promise.all(authPairs.map((authPair) => {
+        emojiAdd = new EmojiAdd(authPair.subdomain, authPair.token);
+        return emojiAdd.upload(program.src);
+      }));
+    }
+
+    if (program.userStats) {
+      return Promise.all(authPairs.map((authPair) => {
+        adminList = new EmojiAdminList(authPair.subdomain, authPair.token);
+        return adminList.get().then(emojiList => {
+          //TODO: update summarizeUser to take single user for consistency?
+          if (program.user) {
+            adminList.summarizeUser(emojiList, program.user);
+          } else {
+            adminList.summarizeSubdomain(emojiList, program.top);
+          }
+        });
+      }));
+    }
+
+    if (program.sync) {
+      if (program.subdomain.length < 2) {
+        return Promise.reject('Sync requires pairs of subdomain / token arguments');
+      }
+
+      return Promise.all(authPairs.map((authPair) => {
+        adminList = new EmojiAdminList(authPair.subdomain, authPair.token);
+        return adminList.get();
+      })).then((lists) => {
+        let diffsToUpload = EmojiAdminList.diff(lists, program.subdomain);
+        Promise.resolve(diffsToUpload);
+      });
+    }
+  } else if (hasValidSrcDstInputs(program)){
+    if (program.sync) {
+
+      let srcPairs = _.zipWith(program.srcSubdomain, program.srcToken, (s, t) => {
+        return {subdomain: s, token: t};
+      });
+      let dstPairs = _.zipWith(program.dstSubdomain, program.dstToken, (s, t) => {
+        return {subdomain: s, token: t};
+      });
+
+      return Promise.all([srcPairs, dstPairs].map((pairs) => {
+        return Promise.all(pairs.map(pair => {
+          adminList = new EmojiAdminList(pair.subdomain, pair.token);
+          return adminList.get();
+        }))
+      })).then(srcDstList => {
+        let srcEmojiLists = srcDstList[0];
+        let dstEmojiLists = srcDstList[1];
+        let diffsToUpload = EmojiAdminList.oneWayDiff(srcEmojiLists, program.srcSubdomain, dstEmojiLists, program.dstSubdomain);
+
+        Promise.resolve(diffsToUpload);
+      });
+    }
+
+  } else {
+    return Promise.reject('At least one subdomain/token pair is required');
   }
 }
 
