7d01b54badca0b7b9de685a53a9a2e77e81f820d
jackellenberger <jellenberger@uchicago.edu>
2018-10-06 15:04:48 -0500

    fixup download bug and too lazy to switch branches

--- emojme-download.js
+++ emojme-download.js
@@ -14,8 +14,8 @@ if (require.main === module) {
 
   Cli.requireAuth(program)
   Cli.allowIoControl(program)
-    .option('--user <value>', 'slack user you\'d like to get stats on. Can be specified multiple times for multiple users.', Cli.list, null)
-    .option('--save', 'create local files of the given subdomain\s emoji')
+    //TODO: re-add --user option to save user admin list without getting stats or saving to disk
+    .option('--save <user>', 'save all of <user>\'s emoji to disk. specify "all" to save all emoji.', Cli.list, [])
     .parse(process.argv)
 
   return download(program.subdomain, program.token, {
@@ -36,8 +36,8 @@ async function download(subdomains, tokens, options) {
   let downloadPromises = authPairs.map(async authPair => {
     let adminList = new EmojiAdminList(...authPair, options.output);
     let emojiList = await adminList.get(options.bustCache);
-    if (options.save)
-      return await EmojiAdminList.save(emojiList, authPair[0], options.user);
+    if (options.save.length > 0)
+      return await EmojiAdminList.save(emojiList, authPair[0], options.save);
 
     return emojiList;
   });
